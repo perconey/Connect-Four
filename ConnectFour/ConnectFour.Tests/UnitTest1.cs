4c278d7925bc61b37defe4cff2bbdfe20187637b
Perconey <perconey@gmail.com>
2018-02-09 20:44:44 +0100

    debug colors

--- ConnectFour/ConnectFour.Tests/UnitTest1.cs
+++ ConnectFour/ConnectFour.Tests/UnitTest1.cs
@@ -35,5 +35,37 @@ namespace ConnectFour.Tests
                 i++;
             }
         }
+        [TestMethod]
+        public void BoardColorCheck()
+        {
+            //Arrange
+            var g = new Game();
+            int i = 0, l = 1;
+
+            //Act
+            var fm = g.FieldsMap;
+            Debug.Write("     ");
+            for (i = 0; i < 7; i++)
+            {
+                Debug.Write(i + 1 + "              ");
+            }
+
+            //Check
+            foreach (var item in fm)
+            {
+                if (i % 7 == 0)
+                {
+                    Debug.Write("\n" + l);
+                    l++;
+                }
+                if (item.Key == 0)
+                    Debug.Write($"[none] ");
+                if (item.Key == 1)
+                    Debug.Write($"[yellow] ");
+                if (item.Key == 2)
+                    Debug.Write($"[red] ");
+                i++;
+            }
+        }
     }
 }
