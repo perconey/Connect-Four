fce9b7091ae8c85b6f470287ddc74fb2817c9056
Perconey <perconey@gmail.com>
2018-02-11 21:51:18 +0100

    Pin adding test

--- ConnectFour/ConnectFour.Tests/UnitTest1.cs
+++ ConnectFour/ConnectFour.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using ConnectFour.Logic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -23,17 +24,7 @@ namespace ConnectFour.Tests
             }
 
             //Check
-            foreach(var item in fm)
-            {
-                if (i % 7 == 0)
-                {
-                    Debug.Write("\n" + l);
-                    l++;
-                }
-
-                Debug.Write($"[{item.Key} , {item.Value}] ");
-                i++;
-            }
+            ShowFields(fm);
         }
         [TestMethod]
         public void BoardColorCheck()
@@ -59,13 +50,55 @@ namespace ConnectFour.Tests
                     l++;
                 }
                 if (item.Key == 0)
-                    Debug.Write($"[none] ");
+                    Debug.Write($"[none]     ");
                 if (item.Key == 1)
-                    Debug.Write($"[yellow] ");
+                    Debug.Write($"[yellow]   ");
                 if (item.Key == 2)
-                    Debug.Write($"[red] ");
+                    Debug.Write($"[red]      ");
                 i++;
             }
         }
+
+        [TestMethod]
+        public void PinAddBlankTest()
+        {
+            //Arrange
+            var g = new Game();
+            int i = 0, l = 1;
+
+            //Act
+            var fm = g.FieldsMap;
+            g.AddPin(2);
+
+            Debug.Write("     ");
+            for (i = 0; i < 7; i++)
+            {
+                Debug.Write(i + 1 + "              ");
+            }
+
+            //Check
+            ShowFields(fm);
+        }
+
+        private void ShowFields(KeyValuePair<int, bool>[,] gameBoard)
+        {
+            int i = 0 , l = 1;
+            foreach (var item in gameBoard)
+            {
+                if (i % 7 == 0)
+                {
+                    Debug.Write("\n" + l);
+                    l++;
+                }
+                if (item.Key == 0)
+                    Debug.Write($"[none]     ");
+                if (item.Key == 1)
+                    Debug.Write($"[yellow]   ");
+                if (item.Key == 2)
+                    Debug.Write($"[red]      ");
+                i++;
+            }
+        }
+
     }
 }
