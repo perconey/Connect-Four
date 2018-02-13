7ceb454ed1601614fba5cf83d84b75e9085e381b
Perconey <perconey@gmail.com>
2018-02-13 21:20:09 +0100

    Arrow commands

--- ConnectFour/ConnectFour/ViewModel/MainWindowViewModel.cs
+++ ConnectFour/ConnectFour/ViewModel/MainWindowViewModel.cs
@@ -1,13 +1,21 @@
-﻿using System.ComponentModel;
+﻿using ConnectFour.Logic;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace ConnectFour.ViewModel
 {
     class MainWindowViewModel : INotifyPropertyChanged
     {
+        public ICommand Column1PinAddClick { get; set; }
+        public ICommand Column2PinAddClick { get; set; }
+        public ICommand Column3PinAddClick { get; set; }
+        public ICommand Column4PinAddClick { get; set; }
+        public ICommand Column5PinAddClick { get; set; }
+        public ICommand Column6PinAddClick { get; set; }
+        public ICommand Column7PinAddClick { get; set; }
 
-
-
-
+        private Game _game = new Game();
+        public Game Game { get => _game; set => _game = value; }
 
         public event PropertyChangedEventHandler PropertyChanged;
         public void NotifyPropertyChanged(string propertyName)
