0b9ea91b050225634bfb384137b62d72a268c01e
Perconey <perconey@gmail.com>
2018-02-14 18:04:02 +0100

    Binding gui image sources

--- ConnectFour/ConnectFour/ViewModel/MainWindowViewModel.cs
+++ ConnectFour/ConnectFour/ViewModel/MainWindowViewModel.cs
@@ -7,6 +7,11 @@ namespace ConnectFour.ViewModel
 {
     class MainWindowViewModel : INotifyPropertyChanged
     {
+
+        private Game _game = new Game();
+        private GameBoardMapper _mapper = new GameBoardMapper();
+        private string[,] mappedLocs = null;
+
         public ICommand Column1PinAddClick { get; set; }
         public ICommand Column2PinAddClick { get; set; }
         public ICommand Column3PinAddClick { get; set; }
@@ -15,11 +20,24 @@ namespace ConnectFour.ViewModel
         public ICommand Column6PinAddClick { get; set; }
         public ICommand Column7PinAddClick { get; set; }
 
-        private Game _game = new Game();
         public Game Game { get => _game; set => _game = value; }
+        public GameBoardMapper Mapper
+        {
+            get => _mapper;
+            set
+            {
+                _mapper = value;
+            }
+        }
 
-        public GameBoardMapper mapper = new GameBoardMapper();
-
+        public string[,] MappedLocs {
+            get => mappedLocs;
+            set
+            {
+                mappedLocs = value;
+                NotifyPropertyChanged("MappedLocs");
+            }
+        }
 
         public MainWindowViewModel()
         {
@@ -30,37 +48,50 @@ namespace ConnectFour.ViewModel
             Column5PinAddClick = new RelayCommand(Column5Click, o => true);
             Column6PinAddClick = new RelayCommand(Column6Click, o => true);
             Column7PinAddClick = new RelayCommand(Column7Click, o => true);
-
+            mappedLocs = Mapper.FileNameMapper;
         }
 
         public void Column1Click(object o)
         {
-            MessageBox.Show("Popup 1");
+            Game.AddPin(1);
+            UpdateMapping();
         }
 
         public void Column2Click(object o)
         {
-            MessageBox.Show("Popup 2");
+            Game.AddPin(2);
+            UpdateMapping();
         }
         public void Column3Click(object o)
         {
-            MessageBox.Show("Popup 3");
+            Game.AddPin(3);
+            UpdateMapping();
         }
         public void Column4Click(object o)
         {
-            MessageBox.Show("Popup 4");
+            Game.AddPin(4);
+            UpdateMapping();
         }
         public void Column5Click(object o)
         {
-            MessageBox.Show("Popup 5");
+            Game.AddPin(5);
+            UpdateMapping();
         }
         public void Column6Click(object o)
         {
-            MessageBox.Show("Popup 6");
+            Game.AddPin(6);
+            UpdateMapping();
         }
         public void Column7Click(object o)
         {
-            MessageBox.Show("Popup 7");
+            Game.AddPin(7);
+            UpdateMapping();
+        }
+
+        private void UpdateMapping()
+        {
+            Mapper.MapToFileName(Game);
+            mappedLocs = Mapper.FileNameMapper;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
